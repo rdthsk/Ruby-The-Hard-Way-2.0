4dd8431eedb31fe374209bf07d87283e95178f62
Rebecca Taylor <rebecca@Rebeccas-MacBook-Pro.local>
2014-07-22 10:18:07 -0700

    Finished the 28th exercise

---
+++ ex28.rb
@@ -0,0 +1,20 @@
+true && true
+false && true
+1 == 1 && 2 == 1
+"test" == "test"
+1 == 1 || 2 != 1
+true && 1 == 1
+false && 0 != 0
+true || 1 == 1
+"test" == "testing"
+1 != 0 && 2 == 1
+"test" != "testing"
+"test" == 1
+not (true && false)
+not (1 == 1 && 0 != 1)
+not (10 == 1 || 1000 == 1000)
+not (1 != 10 || 3 == 4)
+not ("testing" == "testing" && "Zed" == "Cool Guy")
+1 == 1 && not ("testing" == 1 || 1 == 0)
+"chunky" == "bacon" && not (3 == 4 || 3 == 3)
+3 == 3 && not ("testing" == "testing" || "Ruby" == "Fun")
